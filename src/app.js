f2531fe61e49c3463e2294a00eb038af4ffcf1e7
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-02-01 01:41:30 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["My dog", "A plane", "My brother", "My friend"];
+  let action = ["ate", "blew up", "broke", "destroyed"];
+  let what = ["my car", "my homework", "my code", "my computer"];
+  let when = ["yesterday", "tonight", "today", "earlier"];
+
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  let excuse = `${who[randomWho]} ${action[randomAction]} ${what[randomWhat]} ${when[randomWhen]}`;
+
+  document.getElementById("excuse").innerHTML = excuse;
 };
